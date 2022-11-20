3c33f95b48f48ead3757c86cc1e41af94e99f503
Morteza Rastegar <s_qwerty13@live.com>
2022-11-20 19:55:49 +0330

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfWebRTCPlayer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PlayerWindow myPW;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void win_main_Loaded(object sender, RoutedEventArgs e)
+        {
+            myPW = new PlayerWindow();
+            //myPW.Width = Properties.Settings.Default.set_playerWidth;
+            //myPW.Height = Properties.Settings.Default.set_playerHeight;
+            myPW.Top = 0;
+            myPW.Left = 0;
+            myPW.Show();
+
+            Top = myPW.Top + myPW.Height + 50;
+            Left = myPW.Left + myPW.Width + 50;
+        }
+
+        private void win_main_Closed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btn_play_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+            myPW.playLive();
+        }
+
+        private void btn_stop_Click(object sender, RoutedEventArgs e)
+        {
+            myPW.web_player.NavigateToString("");
+        }
+    }
+}
---
+++ PlayerWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Web.WebView2.Core;
+
+namespace WpfWebRTCPlayer
+{
+    /// <summary>
+    /// Interaction logic for PlayerWindow.xaml
+    /// </summary>
+    public partial class PlayerWindow : Window
+    {
+        public PlayerWindow()
+        {
+            Environment.SetEnvironmentVariable("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS", "--autoplay-policy=no-user-gesture-required");
+            InitializeComponent();
+        }
+
+        private async void web_player_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (Properties.Settings.Default.set_playAuto)
+            {
+                playLive();
+            }
+        }
+
+        public async void playLive()
+        {
+            var html = @"
+            <!DOCTYPE html>
+            <html lang=""en"">
+            <head>
+                <meta charset=""UTF-8"">
+                <meta http-equiv=""X-UA-Compatible"" content=""IE=edge"">
+                <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
+                <title>OvenPlayer</title>
+            </head>
+            <body>
+                <div id=""player_id""></div>
+
+                <script>" + Properties.Resources.ovenplayer + @"</script>
+
+                <script>
+                    // Initialize OvenPlayer
+                    const player = OvenPlayer.create('player_id', {
+                        sources: [
+                            {
+                                label: 'label_for_webrtc',
+                                // Set the type to 'webrtc'
+                                type: 'webrtc',
+                                // Set the file to WebRTC Signaling URL with OvenMediaEngine 
+                                file: '" + Properties.Settings.Default.set_serverAddress + @"'
+                            }
+                        ],
+                        expandFullScreenUI: false,
+                        controls: false,
+                        showBigPlayButton: false,
+                        autoStart: true,
+                    });
+                    //setTimeout(player.play(), 1000);
+                    player.toggleFullScreen();
+                </script>
+            </body>
+            </html>
+            ";
+
+            await web_player.EnsureCoreWebView2Async();
+            web_player.NavigateToString(html);
+            //web_player.CoreWebView2.Navigate(@"Assets\Web\index.html");
+        }
+    }
+}
